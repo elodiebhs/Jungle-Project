d5c938eecfacfee277490a4496938c51b0d43cc2
elodiebhs <elodiebouthors@hotmail.com>
2021-03-31 21:48:46 +0000

    add authentification and variable

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,10 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"]
+
   def index
+    @categories = categories.order(id: :desc).all
   end
 
   def new
