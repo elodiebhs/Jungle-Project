b1719e0bc4d651630719a07d2fafb6bca46cbb09
elodiebhs <elodiebouthors@hotmail.com>
2021-03-31 18:17:54 +0000

    Add  Admin Authentication and update ENV

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"]
+                              
+
+  
+  
   def show
   end
 end
+
+
