9b0c9a878f0d15931726fa3474c9e3c15a214134
elodiebhs <elodiebouthors@hotmail.com>
2021-03-31 22:54:14 +0000

    add view, link in header, and form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,12 +4,28 @@ class Admin::CategoriesController < ApplicationController
   password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
-    @categories = categories.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 end
