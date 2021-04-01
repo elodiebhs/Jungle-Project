4ebdbd16608cdeed72ad32a7d42da2a82b5b4152
elodiebhs <elodiebouthors@hotmail.com>
2021-04-01 02:00:19 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+
   # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
     get '/signup' => 'users#new'
