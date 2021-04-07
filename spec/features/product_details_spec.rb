a7e507483656eb100c0ce43b725dde2cc33db305
elodiebhs <elodiebouthors@hotmail.com>
2021-04-07 00:20:15 +0000

    fixing test to add image

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -21,10 +21,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
     first('article.product').find('.btn-default').click()
-
+    save_screenshot()
 
   #   VERIFY
     expect(page).to have_css 'article.product-detail'
-    page.save_screenshot()
+    save_screenshot()
   end
 end
